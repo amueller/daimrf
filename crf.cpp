c2911af0b6c40feb1af10ce6c3ea090544f8eaae
Andy <amueller@ais.uni-bonn.de>
2012-05-17 20:16:20 +0200

    MRF working :)

--- crf.cpp
+++ crf.cpp
@@ -27,38 +27,84 @@ void validate_unaries_edges(PyArrayObject* unaries, PyArrayObject* edges)
        throw runtime_error("Edges must be of size n_edges x 2.");
 }
 
-//PyObject * get_fg_map(vector<Factor> factors){
-//    FactorGraph fg(factors);
-//    size_t maxiter = 10000;
-//    Real   tol = 1e-9;
-//    size_t verb = 1;
+PyObject * mrf(PyArrayObject* unaries, PyArrayObject* edges, PyArrayObject* edge_potentials) {
+    // validate input
+    validate_unaries_edges(unaries, edges);
+    if (PyArray_NDIM(edge_potentials) != 2)
+        throw runtime_error("Edge potentials must be n_classes x n_classes");
+   if (PyArray_TYPE(edge_potentials) != PyArray_FLOAT64)
+       throw runtime_error("Edge potentials must be double.");
 
-//    // Store the constants in a PropertySet object
-//    PropertySet opts;
-//    opts.set("maxiter",maxiter);  // Maximum number of iterations
-//    opts.set("tol",tol);          // Tolerance for convergence
-//    opts.set("verbose",verb);     // Verbosity (amount of output generated)
+    npy_intp* unaries_dims = PyArray_DIMS(unaries);
+    npy_intp* edges_dims = PyArray_DIMS(edges);
+    npy_intp* edge_potential_dims = PyArray_DIMS(edge_potentials);
+    int n_vertices = unaries_dims[0];
+    int n_states = unaries_dims[1];
+    int n_edges = edges_dims[0];
+    if ((edge_potential_dims[0] != n_states) or (edge_potential_dims[1] != n_states))
+        throw runtime_error("Edge potentials must be n_classes x n_classes");
 
-//    BP mp(fg, opts("updates",string("SEQRND"))("logdomain",false)("inference",string("MAXPROD"))("damping",string("0.1")));
-//    mp.init();
-//    mp.run();
-//    vector<size_t> mpstate = mp.findMaximum();
+    cout << "n_vertices: " << n_vertices << " n_states: " << n_states << " n_edges: " << n_edges << endl;
+
+    vector<Var> vars;
+    vector<Factor> factors;
+    vars.reserve(n_vertices);
+
+    // add variables
+    for(size_t i = 0; i < n_vertices; i++)
+        vars.push_back(Var(i, n_states));
+
+    factors.reserve(n_edges + n_vertices);
+    // add unary factors
+    for(size_t i = 0; i < n_vertices; i++){
+        Factor unary_factor(vars[i]);
+        for(size_t j = 0; j < n_states; j++)
+            unary_factor.set(j, *((double*)PyArray_GETPTR2(unaries, i, j)));
+        factors.push_back(unary_factor);
+    }
+    for(size_t e = 0; e < n_edges; e++){
+        int e0 = *((long*)PyArray_GETPTR2(edges, e, 0));
+        int e1 = *((long*)PyArray_GETPTR2(edges, e, 1));
+        Factor pairwise_factor(VarSet(vars[e0], vars[e1]));
+        for (size_t i = 0; i < n_states; i++)
+            for(size_t j = 0; j < n_states; j++){
+                cout << i << " " << j << " " << *((double*)PyArray_GETPTR2(edge_potentials, i, j)) << endl;
+                pairwise_factor.set(i + n_states * j, *((double*)PyArray_GETPTR2(edge_potentials, i, j)));
+            }
+        factors.push_back(pairwise_factor);
+    }
     
-//    //PyArrayObject * map = (PyArrayObject*) PyArray_SimpleNew(1, (npy_intp*)&n_vertices, PyArray_INT);
-//    int n_vertices = fg.nrVars();
-//    PyObject * map = PyArray_SimpleNew(1, (npy_intp*)&n_vertices, PyArray_INT);
-//    for(size_t i = 0; i < n_vertices; i++)
-//        ((int*)PyArray_GETPTR1(map, i))[0] = mpstate[i];
-//    return map;
-//}
+    FactorGraph fg(factors);
+    size_t maxiter = 10000;
+    Real   tol = 1e-9;
+    size_t verb = 1;
+
+    // Store the constants in a PropertySet object
+    PropertySet opts;
+    opts.set("maxiter",maxiter);  // Maximum number of iterations
+    opts.set("tol",tol);          // Tolerance for convergence
+    opts.set("verbose",verb);     // Verbosity (amount of output generated)
+
+    BP mp(fg, opts("updates",string("SEQRND"))("logdomain",false)("inference",string("MAXPROD"))("damping",string("0.1")));
+    mp.init();
+    mp.run();
+    vector<size_t> mpstate = mp.findMaximum();
+    
+    PyObject * map = PyArray_SimpleNew(1, (npy_intp*)&n_vertices, PyArray_INT);
+    if (map == NULL)
+        throw runtime_error("Could not allocate output array.");
+    for(size_t i = 0; i < n_vertices; i++){
+        ((int*)PyArray_GETPTR1(map, i))[0] = mpstate[i];
+    }
+    return map;
+}
+
 
 PyObject * potts_crf(PyArrayObject* unaries, PyArrayObject* edges, double edge_strength) {
     // validate input
     validate_unaries_edges(unaries, edges);
     npy_intp* unaries_dims = PyArray_DIMS(unaries);
     npy_intp* edges_dims = PyArray_DIMS(edges);
-    if (edges_dims[1] != 2)
-        throw runtime_error("Edges must be of size n_edges x 2.");
     int n_vertices = unaries_dims[0];
     int n_states = unaries_dims[1];
     int n_edges = edges_dims[0];
@@ -85,9 +131,6 @@ PyObject * potts_crf(PyArrayObject* unaries, PyArrayObject* edges, double edge_s
         int e0 = *((long*)PyArray_GETPTR2(edges, e, 0));
         int e1 = *((long*)PyArray_GETPTR2(edges, e, 1));
         Factor pairwise_factor = createFactorPotts(vars[e0], vars[e1], edge_strength);
-        //for (size_t i = 0; i < n_states; i++)
-            //for(size_t j = 0; j < n_states; j++)
-                //pairwise_factor.set(i + n_states * j, i==j? edge_strength : 0);
         factors.push_back(pairwise_factor);
     }
     
@@ -107,8 +150,9 @@ PyObject * potts_crf(PyArrayObject* unaries, PyArrayObject* edges, double edge_s
     mp.run();
     vector<size_t> mpstate = mp.findMaximum();
     
-    //PyArrayObject * map = (PyArrayObject*) PyArray_SimpleNew(1, (npy_intp*)&n_vertices, PyArray_INT);
     PyObject * map = PyArray_SimpleNew(1, (npy_intp*)&n_vertices, PyArray_INT);
+    if (map == NULL)
+        throw runtime_error("Could not allocate output array.");
     for(size_t i = 0; i < n_vertices; i++){
         ((int*)PyArray_GETPTR1(map, i))[0] = mpstate[i];
     }
@@ -124,5 +168,6 @@ BOOST_PYTHON_MODULE(daicrf){
 	boost::python::converter::registry::insert(
 	    &extract_pyarray, boost::python::type_id<PyArrayObject>());
     boost::python::def("potts_crf", potts_crf);
+    boost::python::def("mrf", mrf);
     import_array();
 }
--- example.py
+++ example.py
@@ -1,4 +1,4 @@
-from daicrf import potts_crf
+from daicrf import potts_crf, mrf
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -7,11 +7,11 @@ x[:, 5:] = -1
 x_noisy = x + np.random.normal(0, 0.8, size=x.shape)
 x_thresh = x_noisy > .0
 plt.subplot(141)
-plt.imshow(x)
+plt.imshow(x, interpolation='nearest')
 plt.subplot(142)
-plt.imshow(x_noisy)
+plt.imshow(x_noisy, interpolation='nearest')
 plt.subplot(143)
-plt.imshow(x_thresh)
+plt.imshow(x_thresh, interpolation='nearest')
 
 inds = np.arange(x.size).reshape(x.shape)
 horz = np.c_[inds[:, :-1].ravel(), inds[:, 1:].ravel()]
@@ -25,5 +25,8 @@ unaries = np.c_[np.exp(-unaries), np.exp(unaries)]
 
 result = potts_crf(unaries, edges, 1.1)
 plt.subplot(144)
-plt.imshow(result.reshape(x.shape))
+plt.imshow(result.reshape(x.shape), interpolation='nearest')
+
+result_mrf = mrf(unaries, edges, np.exp(np.eye(2) * 1.1))
+plt.matshow(result_mrf.reshape(x.shape), interpolation='nearest')
 plt.show()
