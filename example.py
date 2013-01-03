7b53775309232b94a0debfc5febc6fa3b3f962e7
Andreas Mueller <amueller@ais.uni-bonn.de>
2013-01-03 21:31:01 +0100

    Titles for the examples

--- example.py
+++ example.py
@@ -2,6 +2,7 @@ from daicrf import potts_mrf, mrf
 import matplotlib.pyplot as plt
 import numpy as np
 
+
 def compare_algorithms():
     x = np.ones((10, 10))
     x[:, 5:] = -1
@@ -17,24 +18,30 @@ def compare_algorithms():
     unaries = np.c_[np.exp(-unaries), np.exp(unaries)]
     pairwise = np.exp(np.eye(2) * 4.1)
     algorithms = ["maxprod", "gibbs", "jt", "trw", "treeep"]
-    fix, axes = plt.subplots(len(algorithms))
+    fix, axes = plt.subplots(1, len(algorithms))
     for ax, alg in zip(axes, algorithms):
         result_mrf = mrf(unaries, edges, pairwise, verbose=1, alg=alg)
         ax.matshow(result_mrf.reshape(x.shape))
+        ax.set_title(alg)
+        ax.set_xticks(())
+        ax.set_yticks(())
     plt.show()
 
 
 def example_binary():
     x = np.ones((10, 10))
     x[:, 5:] = -1
-    x_noisy = x + np.random.normal(0, 0.8, size=x.shape)
+    x_noisy = x + np.random.normal(0, .8, size=x.shape)
     x_thresh = x_noisy > .0
-    plt.subplot(141)
-    plt.imshow(x, interpolation='nearest')
-    plt.subplot(142)
-    plt.imshow(x_noisy, interpolation='nearest')
-    plt.subplot(143)
-    plt.imshow(x_thresh, interpolation='nearest')
+
+    fig, axes = plt.subplots(1, 5)
+    axes[0].set_title("Original")
+    axes[0].matshow(x)
+    axes[1].set_title("binary")
+    axes[1].matshow(x_noisy)
+
+    axes[2].set_title("thresholded")
+    axes[2].matshow(x_thresh)
 
     inds = np.arange(x.size).reshape(x.shape).astype(np.int64)
     horz = np.c_[inds[:, :-1].ravel(), inds[:, 1:].ravel()]
@@ -45,12 +52,17 @@ def example_binary():
     unaries = x_noisy.ravel()
     unaries = np.c_[np.exp(-unaries), np.exp(unaries)]
 
+    axes[3].set_title("Potts MRF")
     result = potts_mrf(unaries, edges, 1.1, verbose=1)
-    plt.subplot(144)
-    plt.imshow(result.reshape(x.shape), interpolation='nearest')
+    axes[3].matshow(result.reshape(x.shape))
 
-    result_mrf = mrf(unaries, edges, np.exp(np.eye(2) * 1.1), verbose=1, alg="trw")
-    plt.matshow(result_mrf.reshape(x.shape), interpolation='nearest')
+    result_mrf = mrf(unaries, edges, np.exp(np.eye(2) * 1.1), verbose=1,
+                     alg="trw")
+    axes[4].set_title("MRF")
+    axes[4].matshow(result_mrf.reshape(x.shape))
+    for ax in axes:
+        ax.set_xticks(())
+        ax.set_yticks(())
     plt.show()
 
 
@@ -75,16 +87,21 @@ def example_multinomial():
     binaries[0, -1] = 0
     print(binaries)
     result_mrf = mrf(unaries_noisy, edges, np.exp(binaries), alg="jt")
-    plt.subplot(141)
-    plt.imshow(x, interpolation="nearest")
-    plt.subplot(142)
-    plt.imshow(x_thresh, interpolation="nearest")
-    plt.subplot(143)
-    plt.imshow(result.reshape(x.shape), interpolation="nearest")
-    plt.subplot(144)
-    plt.imshow(result_mrf.reshape(x.shape), interpolation="nearest")
+    plot, axes = plt.subplots(1, 4)
+    axes[0].set_title("original")
+    axes[0].matshow(x)
+    axes[1].set_title("thresholded")
+    axes[1].matshow(x_thresh)
+    axes[1].set_title("Potts MRF")
+    axes[2].matshow(result.reshape(x.shape))
+    axes[1].set_title("MRF")
+    axes[3].matshow(result_mrf.reshape(x.shape))
+    for ax in axes:
+        ax.set_xticks(())
+        ax.set_yticks(())
     plt.show()
 
 if __name__ == "__main__":
     #example_binary()
+    #example_multinomial()
     compare_algorithms()
